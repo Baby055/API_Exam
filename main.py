f4af250f2df02b32a3852cef14fb18feade297bc
Baby055 <hei.sombiniaina@gmail.com>
2025-09-03 12:13:41 +0300

    dernier commit

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Dict
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+cars_db: Dict[str, dict] = {}
+
+
+class Characteristic(BaseModel):
+    max_speed: float
+    max_fuel_capacity: float
+
+class Car(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+# a
+@app.get("/ping")
+def ping():
+    return "pong"
+
+# b
+@app.post("/cars", status_code=201)
+def create_car(car: Car):
+    cars_db[car.identifier] = car.dict()
+    return car
+
+# c
+@app.get("/cars", response_model=List[Car])
+def get_cars():
+    return list(cars_db.values())
+
+# d
+@app.get("/cars/{id}", response_model=Car)
+def get_car(id: str):
+    if id not in cars_db:
+        raise HTTPException(status_code=404, detail=f"Car with id '{id}' not found.")
+    return cars_db[id]
+
